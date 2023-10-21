9b23bdab1f9db0bf8d1dde4ff40c5fb697c12f2a
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-10-21 21:13:52 +0300

    Seed positions and departments

---
+++ HumanCapitalManagementApp.Data/Migrations/20231021181307_SeedPositionsAndDepartments.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HumanCapitalManagementApp.Migrations
+{
+    public partial class SeedPositionsAndDepartments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Architect" },
+                    { 2, "Developer" },
+                    { 3, "UI/UX Designer" },
+                    { 4, "Tester" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
